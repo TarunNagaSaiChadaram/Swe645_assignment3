7640641a10b3fa2a96996c3d6c5a16c85a45137a
AkshayaReddy25 <96285664+AkshayaReddy25@users.noreply.github.com>
2025-04-16 23:22:51 -0400

    Update StusurveyApplicationTests.java

--- src/test/java/com/tarun/stusurvey/StusurveyApplicationTests.java
+++ src/test/java/com/tarun/stusurvey/StusurveyApplicationTests.java
@@ -1,6 +1,4 @@
-# -Done By,Samanvitha Matta,G01252738-->
-#<!--Akshaya Reddy Dundigalla,G01482843-->
-#<!--Tarun Naga Sai Chadaram,G01445928-->
+
 package com.tarun.stusurvey;
 
 import org.junit.jupiter.api.Test;
